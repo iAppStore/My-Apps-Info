aff0fad5cfc4f074939e374a8071243ad8e9e7c0
mohammad mugish <mohammad.mugish@gmail.com>
2022-01-28 02:11:19 +0530

    Initial commit

---
+++ iAppStore/iAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAppStoreApp.swift
+//  iAppStore
+//
+//  Created by mohammad mugish on 28/01/22.
+//
+
+import SwiftUI
+
+@main
+struct iAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
